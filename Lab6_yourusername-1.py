78b39e4453e0abe034f99174106831187306f49b
Gnalen Mara <gnalenmara80@gmail.com>
2026-02-19 21:55:17 -0500

    Initialize Lab 6 project and add file structure

---
+++ Lab6_yourusername-1.py
@@ -0,0 +1,10 @@
+"""
+Program Name: Lab6_yourusername-1.py
+Author: Gnalen Mara
+purpose:  This program verifies user login information using a dictionary 
+and displays the correct security level.
+Starter Code: No starter code used
+Date: 2/19/2026
+
+"""
+# Create a dictionary to store user login information
